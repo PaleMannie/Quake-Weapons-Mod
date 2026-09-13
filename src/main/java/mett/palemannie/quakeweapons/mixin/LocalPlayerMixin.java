00da6312dcebdcca948e3d2e3a6b71cc36447764
PaleMannie <einalex44@gmail.com>
2026-09-13 16:38:12 +0200

    block break while shooting fix

--- src/main/java/mett/palemannie/quakeweapons/mixin/LocalPlayerMixin.java
+++ src/main/java/mett/palemannie/quakeweapons/mixin/LocalPlayerMixin.java
@@ -10,25 +10,19 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin {
 
-    @ModifyReturnValue(
-            method = "itemUseSpeedMultiplier",
-            at = @At("RETURN")
-    )
+    @ModifyReturnValue(method = "itemUseSpeedMultiplier", at = @At("RETURN"))
     private float quakeweapons$noUseSlowForWeapons(float original) {
         LocalPlayer self = (LocalPlayer) (Object) this;
         ItemStack stack = self.getUseItem();
 
         if (stack.getItem() instanceof AbstractWeapon) {
-            return 1.0F;
+            return 1f;
         }
 
         return original;
     }
 
-    @ModifyReturnValue(
-            method = "isSlowDueToUsingItem",
-            at = @At("RETURN")
-    )
+    @ModifyReturnValue(method = "isSlowDueToUsingItem", at = @At("RETURN"))
     private boolean quakeweapons$allowSprintWhileUsingWeapons(boolean original) {
         LocalPlayer self = (LocalPlayer) (Object) this;
         ItemStack stack = self.getUseItem();
